8e56bbbb1e76b6d5eae25c63f4141aa1e8fa79a3
[SW7] 이수빈 <dltnqls3267@naver.com>
2025-04-25 17:08:23 +0900

    docs:is_deleted 추가

--- backend/src/types/auth.ts
+++ backend/src/types/auth.ts
@@ -1,7 +1,8 @@
 export interface Signup {
     email: string;
     password: string;
-    is_member: boolean;//기본값 = false
+    is_member: boolean; // 멤버십 유무 판단(기본값 = false)
+    is_deleted: boolean; //탈퇴한 유저 판단(기본값 = false)
 }
 export interface Login {
     email: string;
--- backend/src/types/user.ts
+++ backend/src/types/user.ts
@@ -2,4 +2,5 @@ export interface UpdateUser {
     email?: string;
     password?: string;
     is_member?: boolean;
+    is_deleted?: boolean;
 }
