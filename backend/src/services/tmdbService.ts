fb6690624a6c0a1e1d1b575b2f23d877d503f28b
DDongHyun00 <117076978+DDongHyun00@users.noreply.github.com>
2025-04-28 09:41:40 +0900

    feat:movie type 정의

---
+++ backend/src/services/tmdbService.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { Movie } from "../types/movie";
+
+const API_KEY = process.env.TMDB_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const fetchPopularMovies = async () => {
+  const response = await axios.get<{ results: Movie[] }>(
+    `${BASE_URL}/movie/popular`,
+    {
+      params: {
+        api_key: API_KEY,
+        language: "ko-KR",
+        page: 1,
+      },
+    }
+  );
+
+  return response.data.results;
+};
+    
