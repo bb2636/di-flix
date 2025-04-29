2306d2d192cfe6e9dc51df7af58b19c8ecccd097
[SW7] 이수빈 <dltnqls3267@naver.com>
2025-04-29 17:48:58 +0900

    fix:contentController 리펙토링 & 회원가입,로그인 프론트

---
+++ frontend/src/apis/userApi.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { SignupRequest, LoginRequest } from "../types/user";
+
+//회원가입 api
+export const signup = (signupData: SignupRequest) => {
+    return axios.post("/users/signup", signupData);
+  };
+
+// 로그인 API
+export const login = (loginData: LoginRequest) => {
+    return axios.post("/users/login", loginData);
+  };
