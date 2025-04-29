124e608bf62299cf04c5028174e6b197463cbf54
[SW7] 이수빈 <dltnqls3267@naver.com>
2025-04-29 13:38:27 +0900

    fix:임시 멤버십 결제 요청> 성공

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -9,9 +9,9 @@ import { verifyToken } from "../middlewares/login-required";
 
 const authrouter = express.Router();
 
-authrouter.post("/users/signup", register);
-authrouter.post("/users/login", login);
-authrouter.post("/users/logout", logout);
-authrouter.post("/users/withdraw", verifyToken, withdraw);
+authrouter.post("/signup", register);
+authrouter.post("/login", login);
+authrouter.post("/logout", logout);
+authrouter.post("/withdraw", verifyToken, withdraw);
 
 export default authrouter;
