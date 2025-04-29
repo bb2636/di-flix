9a81cfc1d0c65167e3e6c832d4478c8bf35c80df
DDongHyun00 <117076978+DDongHyun00@users.noreply.github.com>
2025-04-29 17:51:35 +0900

    feat: repush

--- backend/src/routes/contentRouter.ts
+++ backend/src/routes/contentRouter.ts
@@ -8,7 +8,7 @@ import { verifyToken } from "../middlewares/login-required";
 const contentRouter = Router();
 
 // 영화 목록 조회 (프론트에 영화 목록 뿌리기)
-contentRouter.get("/", verifyToken, searchMovieTmDB);
+contentRouter.get("/", searchMovieTmDB);
 
 // 상세 조회 (멤버만 가능)
 contentRouter.get("/:movie_id", verifyToken, getContentDetail);
--- backend/src/services/tmdbService.ts
+++ backend/src/services/tmdbService.ts
@@ -6,18 +6,15 @@ const TMDB_BASE_URL = process.env.TMDB_BASE_URL;
 // 영화 목록 받아오기
 export const fetchMovies = async (page: number = 1) => {
   try {
-    const response = await axios.get(
-      `${process.env.TMDB_BASE_URL}/movie/popular`,
-      {
-        params: {
-          api_key: TMDB_API_KEY, // TMDB API 키
-          language: "ko-KR", // 한국어로 결과 반환
-          page: page, // 페이지 번호
-        },
+    const response = await axios.get(`${TMDB_BASE_URL}/movie/popular`, {
+      params: {
+        api_key: TMDB_API_KEY, // TMDB API 키
+        language: "ko-KR", // 한국어로 결과 반환
+        page: page, // 페이지 번호
       },
-    );
+    });
 
-    return response.data.results.json; // 영화 목록 반환
+    return response.data.results; // 영화 목록 반환
   } catch (error) {
     console.error("TMDB에서 영화 목록 가져오기 실패:", error);
     throw new Error("TMDB에서 영화 목록 가져오기 실패");
