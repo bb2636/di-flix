a4e3e136fa759cb30f09d4e362ff824687990e5f
김동현 <117076978+DDongHyun00@users.noreply.github.com>
2025-04-30 10:05:50 +0900

    Revert "Diflix 52"

--- backend/src/routes/contentRouter.ts
+++ backend/src/routes/contentRouter.ts
@@ -1,20 +1,10 @@
 import { Router } from "express";
-import {
-  getContentDetail,
-  searchMovieTmDB,
-  searchGenreMovieTmDB,
-} from "../controllers/contentController";
+import { getContentDetail } from "../controllers/contentController";
 import { verifyToken } from "../middlewares/login-required";
 
 const contentRouter = Router();
 
-// 영화 목록 조회 (프론트에 영화 목록 뿌리기)
-contentRouter.get("/", searchMovieTmDB);
-
-// 장르별 영화 목록 조회
-contentRouter.get("/genre/:genre_id", searchGenreMovieTmDB);
-
 // 상세 조회 (멤버만 가능)
-contentRouter.get("/:movie_id", verifyToken, getContentDetail);
+contentRouter.get("/content/:movie_id", verifyToken, getContentDetail);
 
 export default contentRouter;
